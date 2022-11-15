cbc2ec754b26194c6aeb9df7778a9f8ced00a44a
Henning <84560092+TheHonning@users.noreply.github.com>
2022-11-15 16:20:28 +0100

    Added questions and a fast Version of the homework

---
+++ Homework 03/mnist_keras.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
+import numpy as np
+
+######################################################
+n_epochs = 5
+eta = 0.001
+n_neurons_hidden_layer = 15
+batch_size = 16
+
+######################################################
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data() #60000 images for train, 10000 images for test
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)), #input layer with 28x28 = 784 neurons
+  tf.keras.layers.Dense(n_neurons_hidden_layer, activation='relu'), #Dense hidden layer with n_neuron_hidden_layer neurons
+  tf.keras.layers.Dense(10) #Output layer without Softmax!
+])
+
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+opt = keras.optimizers.Adam(learning_rate=eta)
+
+model.compile(optimizer=opt,
+              loss=loss_fn,
+              metrics=['accuracy'])
+
+history = model.fit(x_train, y_train, epochs=n_epochs, batch_size=batch_size, validation_data=(x_test, y_test)) #Train and Test 
+model.evaluate(x_test,  y_test, verbose=2)
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_loss'])
+plt.plot(history.history['val_accuracy'])
+plt.grid()
+plt.title('model performance')
+plt.ylabel('Value')
+plt.xlabel('Epoch')
+plt.legend(['Train loss', 'Train accuracy', 'Test loss', 'Test accuracy'], loc='upper left')
+plt.show()
---
+++ Homework 03/questions.md
@@ -0,0 +1,7 @@
+## How many Training and Test images are there?
+    - Test: 60000
+    - Train: 10000
+## What's the Shape?
+    - 28,28,1 -> An image with 28 x 28 pixels
+## Whats the range of pixelvalues?
+    - from 0 to 255 (uint8 = 2**8 = 256 -> 0 to 255)
