8c4ff3a7e17ff8d0fbad572287fd5e3674e1dc54
Zykone <103261294+Zykones@users.noreply.github.com>
2022-11-27 10:39:09 +0100

    Update hw_4.py

--- Homework 04/hw_4.py
+++ Homework 04/hw_4.py
@@ -1,102 +1,12 @@
 import tensorflow_datasets as tfds
 import tensorflow as tf
-import numpy
-import os
+import tqdm
+import os           #do we really need this?
 import datetime
 
-class TwinMNISTModel(tf.keras.Model):
-
-    # 1. constructor
-    def __init__(self):
-        super().__init__()
-        # inherit functionality from parent class
-
-        # optimizer, loss function and metrics
-        self.metrics_list = [tf.keras.metrics.BinaryAccuracy(),
-                             tf.keras.metrics.Mean(name="loss")]
-        
-        self.optimizer = tf.keras.optimizers.Adam()
-        
-        self.loss_function = tf.keras.losses.BinaryCrossentropy()
-        
-        # layers to be used
-        self.dense1 = tf.keras.layers.Dense(32, activation=tf.nn.relu)
-        self.dense2 = tf.keras.layers.Dense(32, activation=tf.nn.relu)
-        
-        self.out_layer = tf.keras.layer.Dense(1,activation=tf.nn.sigmoid)
-        
-        
-        
-        
-    # 2. call method (forward computation)
-    #@tf.function
-    def call(self, images, training=False):
-        img1, img2 = images
-        
-        img1_x = self.dense1(img1)
-        img1_x = self.dense2(img1_x)
-        
-        img2_x = self.dense1(img2)
-        img2_x = self.dense2(img2_x)
-        
-        combined_x = tf.concat([img1_x, img2_x ], axis=1)
-        
-        return self.out_layer(combined_x)
-
-
-
-    # 3. metrics property
-    @property
-    def metrics(self):
-        return self.metrics_list
-        # return a list with all metrics in the model
-
-
-
-    # 4. reset all metrics objects
-    def reset_metrics(self):
-        for metric in self.metrics:
-            metric.reset_states()
-
-
-
-    # 5. train step method
-    def train_step(self, data):
-        img1, img2, label = data
-        
-        with tf.GradientTape() as tape:
-            output = self((img1, img2), training=True)
-            loss = self.loss_function(label, output)
-            
-        gradients = tape.gradient(loss, self.trainable_variables)
-        self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
-
-        #something with the metrics object, need to google
-        self.metrics_list[0].update_state(label, output)
-
-        # What exactly does the "mean" loss capture in this model? 
-        self.metrics_list[1].update_state(loss)
-        
-        # update the state of the metrics according to loss
-        # return a dictionary with metric names as keys and metric results as values
-
-        # Not sure if .numpy() is correct in this context
-        return {"Accuracy" : self.metrics_list[0].result().numpy(), "Loss" : self.metrics_list[1].result().numpy()}
-
-    # 6. test_step method
-    def test_step(self, data):
-        # same as train step (without parameter updates)
-        img1, img2, label = data
-        output = self((img1, img2), training=False)
-        loss = self.loss_function(label, output)
-        self.metrics_list[0].update_state(label, output)
-
-        # What exactly does the "mean" of the loss capture in this model? 
-        self.metrics_list[1].update_state(loss)
-
-        # Not sure if .numpy() is correct in this context
-        return {"Accuracy" : self.metrics_list[0].result().numpy(), "Loss" : self.metrics_list[1].result().numpy()}
-
+mnist = tfds.load("mnist", split =["train","test"], as_supervised=True)
+train_ds = mnist[0]
+val_ds = mnist[1]
 
 def prepare_data(data, batch_size=32):
     # image should be float
@@ -119,35 +29,197 @@ def prepare_data(data, batch_size=32):
     zipped_ds = zipped_ds.prefetch(tf.data.AUTOTUNE)
     return zipped_ds
 
-def training_loop(model, train_ds, test_ds, train_summery_writer, validation_summery_writer, path,  epochs=10):
-    for i in range(epochs):
-        pass
-        # 2. train steps on all batches in the training data
-        # 3. log and print training metrics
-        with train_summery_writer.as_default():
-            pass
+train_ds = prepare_data(train_ds, batch_size=32)
+test_ds = prepare_data(test_ds, batch_size=32)
 
-        # 4. reset metric objects
-        # 5. evaluate on validation data
-        # 6. log validation metrics
-        with validation_summery_writer.as_default():
-            pass
-        # 7. reset metric objects
-    # 8. save model weights
+class TwinMNISTModel(tf.keras.Model):
+
+    # 1. constructor
+    def __init__(self):
+        super().__init__()
+        # inherit functionality from parent class
+
+        # optimizer, loss function and metrics
+        self.metrics_list = [tf.keras.metrics.BinaryAccuracy(),
+                             tf.keras.metrics.Mean(name="loss")]
+        
+        self.optimizer = tf.keras.optimizers.Adam()
+        
+        self.loss_function = tf.keras.losses.BinaryCrossentropy()
+        
+        # layers to be used
+        self.dense1 = tf.keras.layers.Dense(128, activation=tf.nn.relu)
+        self.dense2 = tf.keras.layers.Dense(128, activation=tf.nn.relu)
+        
+        self.dense3 = tf.keras.layers.Dense(128, activation=tf.nn.relu)
+        
+        self.out_layer = tf.keras.layer.Dense(1,activation=tf.nn.sigmoid)
+        
+           
+    # 2. call method (forward computation)
+    #@tf.function
+    def call(self, images, training=False):
+        img1, img2 = images
+        
+        img1_x = self.dense1(img1)
+        img1_x = self.dense2(img1_x)
+        
+        img2_x = self.dense1(img2)
+        img2_x = self.dense2(img2_x)
+        
+        combined_x = tf.concat([img1_x, img2_x ], axis=1)
+        combined_x = self.dense3(combined_x)
+        
+        return self.out_layer(combined_x)
     
-(train_ds, test_ds), ds_info = tfds.load('mnist', split =['train', 'test'], as_supervised=True, with_info=True)
 
-train_ds = prepare_data(train_ds)
-test_ds = prepare_data(test_ds)
+    # 3. metrics property
+    @property
+    def metrics(self):
+        return self.metrics_list
+        # return a list with all metrics in the model
 
-for img1, img2, label in train_ds.take(1):
-    print(img1.shape, img2.shape, label.shape)
+
+    # 4. reset all metrics objects
+    def reset_metrics(self):
+        for metrics in self.metrics:
+            metrics.reset_states()
+
+
+    # 5. train step method
+    def train_step(self, data):
+        img1, img2, label = data
+        
+        with tf.GradientTape() as tape:
+            output = self((img1, img2), training=True)
+            loss = self.loss_function(label, output)
+            
+        gradients = tape.gradient(loss, self.trainable_variables)
+        self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+
+        #update metrics according to loss
+        self.metrics[0].update_state(label, output)
+        self.metrics[1].update_state(loss)
+
+        # What exactly does the "mean" loss capture in this model? 
+        
+        # return a dictionary with metric names as keys and metric results as values
+        return {m.name : m.result() for m in self.metrics}
+       
+
+    # 6. test_step method
+    @tf.function
+    def test_step(self, data):
+        # same as train step (without parameter updates)
+        img1, img2, label = data
+        
+        output = self((img1, img2), training=False)
+        loss = self.loss_function(label, output)
+        self.metrics[0].update_state(label, output)
+        self.metrics[1].update_state(loss)
+
+        return {m.name : m.result() for m in self.metrics}
+
+
+def training_loop(model, train_ds, val_ds, start_epoch,
+                  epochs, train_summary_writer, 
+                  val_summary_writer, save_path):
+
+    # 1. iterate over epochs
+    for e in range(start_epoch, epochs):
+
+        # 2. train steps on all batches in the training data
+        for data in tqdm.tqdm(train_ds, position=0, leave=True):
+            metrics = model.train_step(data)
+
+        # 3. log and print training metrics
+
+        with train_summary_writer.as_default():
+            # for scalar metrics:
+            for metric in model.metrics:
+                    tf.summary.scalar(f"{metric.name}", metric.result(), step=e)
+            # alternatively, log metrics individually (allows for non-scalar metrics such as tf.keras.metrics.MeanTensor)
+            # e.g. tf.summary.image(name="mean_activation_layer3", data = metrics["mean_activation_layer3"],step=e)
+        
+        #print the metrics
+        print([f"{key}: {value.numpy()}" for (key, value) in metrics.items()])
+        
+        # 4. reset metric objects
+        model.reset_metrics()
+
+
+        # 5. evaluate on validation data
+        for data in val_ds:
+            metrics = model.test_step(data)
+        
+
+        # 6. log validation metrics
+
+        with val_summary_writer.as_default():
+            # for scalar metrics:
+            for metric in model.metrics:
+                    tf.summary.scalar(f"{metric.name}", metric.result(), step=e)
+            # alternatively, log metrics individually (allows for non-scalar metrics such as tf.keras.metrics.MeanTensor)
+            # e.g. tf.summary.image(name="mean_activation_layer3", data = metrics["mean_activation_layer3"],step=e)
+            
+        print([f"val_{key}: {value.numpy()}" for (key, value) in metrics.items()])
+        # 7. reset metric objects
+        model.reset_metrics()
+        
+    # 8. save model weights if save_path is given
+    if save_path:
+        model.save_weights(save_path)
+    
+
 
 ### Summary Writers 
-current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+def create_summary_writers(config_name):
+    
+    # Define where to save the logs
+    # along with this, you may want to save a config file with the same name so you know what the hyperparameters were used
+    # alternatively make a copy of the code that is used for later reference
+    
+    current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 
-train_log_path = f"logs/train/{current_time}"
-val_log_path = f"logs/val/{current_time}"
+    train_log_path = f"logs/{config_name}/{current_time}/train"
+    val_log_path = f"logs/{config_name}/{current_time}/val"
 
-train_summary_writer = tf.summary.create_file_writer(train_log_path)
-val_summary_writer = tf.summary.create_file_writer(val_log_path)
+    # log writer for training metrics
+    train_summary_writer = tf.summary.create_file_writer(train_log_path)
+
+    # log writer for validation metrics
+    val_summary_writer = tf.summary.create_file_writer(val_log_path)
+    
+    return train_summary_writer, val_summary_writer
+
+train_summary_writer, val_summary_writer = create_summary_writers(config_name="RUN1")
+
+
+# 1. instantiate model
+model = TwinMNISTModel()
+
+# 2. choose a path to save the weights
+save_path = "trained_model_RUN1"
+
+# 2. pass arguments to training loop function
+
+training_loop(model=model,
+    train_ds=train_ds,
+    val_ds=val_ds,
+    start_epoch=0,
+    epochs=10,
+    train_summary_writer=train_summary_writer,
+    val_summary_writer=val_summary_writer,
+    save_path=save_path)
+
+
+# load the model:
+fresh_model = TwinMNISTModel()
+
+# build the model's parameters by calling it on input
+for img1,img2,label in train_ds:
+    fresh_model((img1,img2));
+    break
+
+# load the saved weights
+model = fresh_model.load_weights(save_path)
