1f5f86175a47d9cc24f2b2044f80cb1e40e7a502
Henning <henningter@googlemail.com>
2022-11-28 17:27:42 +0100

    Create cnn_keras.py
    prototype for hw 05, network is pretty big for the dataset, looking to scale it down until Wednesday

---
+++ Homework 05/cnn_keras.py
@@ -0,0 +1,77 @@
+import keras
+from keras import datasets
+from keras import layers
+from keras.models import Sequential
+from keras.utils import np_utils
+import matplotlib.pyplot as plt
+
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+
+# Converting the pixels data to float type
+train_images = train_images.astype('float32')
+test_images = test_images.astype('float32')
+ 
+# Standardizing (255 is the total number of pixels an image can have)
+train_images = train_images / 255
+test_images = test_images / 255 
+
+# One hot encoding the target class (labels)
+num_classes = 10
+train_labels = np_utils.to_categorical(train_labels, num_classes)
+test_labels = np_utils.to_categorical(test_labels, num_classes)
+
+# Creating a sequential model and adding layers to it
+
+model = Sequential()
+
+model.add(layers.Conv2D(32, (3,3), padding='same', activation='relu', input_shape=(32,32,3)))
+model.add(layers.BatchNormalization())
+model.add(layers.Conv2D(32, (3,3), padding='same', activation='relu'))
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPooling2D(pool_size=(2,2)))
+model.add(layers.Dropout(0.3))
+
+model.add(layers.Conv2D(64, (3,3), padding='same', activation='relu'))
+model.add(layers.BatchNormalization())
+model.add(layers.Conv2D(64, (3,3), padding='same', activation='relu'))
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPooling2D(pool_size=(2,2)))
+model.add(layers.Dropout(0.5))
+
+model.add(layers.Conv2D(128, (3,3), padding='same', activation='relu'))
+model.add(layers.BatchNormalization())
+model.add(layers.Conv2D(128, (3,3), padding='same', activation='relu'))
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPooling2D(pool_size=(2,2)))
+model.add(layers.Dropout(0.5))
+
+model.add(layers.Flatten())
+model.add(layers.Dense(128, activation='relu'))
+model.add(layers.BatchNormalization())
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(num_classes, activation='softmax'))    # num_classes = 10
+
+# Checking the model summary
+model.summary()
+
+model.compile(optimizer='adam', loss=keras.losses.categorical_crossentropy, metrics=['accuracy'])
+
+history = model.fit(train_images, train_labels, batch_size=64, epochs=100, validation_data=(test_images, test_labels))
+
+# Loss curve
+plt.figure(figsize=[6,4])
+plt.plot(history.history['loss'], 'black', linewidth=2.0)
+plt.plot(history.history['val_loss'], 'green', linewidth=2.0)
+plt.legend(['Training Loss', 'Validation Loss'], fontsize=14)
+plt.xlabel('Epochs', fontsize=10)
+plt.ylabel('Loss', fontsize=10)
+plt.title('Loss Curves', fontsize=12)
+
+# Accuracy curve
+plt.figure(figsize=[6,4])
+plt.plot(history.history['accuracy'], 'black', linewidth=2.0)
+plt.plot(history.history['val_accuracy'], 'blue', linewidth=2.0)
+plt.legend(['Training Accuracy', 'Validation Accuracy'], fontsize=14)
+plt.xlabel('Epochs', fontsize=10)
+plt.ylabel('Accuracy', fontsize=10)
+plt.title('Accuracy Curves', fontsize=12)
