61b26929f5f0b8ad3a4343272849756518eb1145
ltegge <ltegge@uos.de>
2022-11-30 13:42:33 +0100

    hw 5 not working

--- hw_5.py
+++ hw_5.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import tensorflow_datasets as tfds
+import numpy as np
 
 #load cifar 10 dataset
 train_ds, test_ds = tfds.load("cifar10", split=["train", "test"], as_supervised=True)
@@ -29,3 +30,84 @@ def prepare_data(cifar10):
 
     return cifar10
 
+
+def Cifar10Model(tf.keras.Model):
+    def __init__(self, neurons):
+        super(Cifar10Model, self).__init__()
+        self.convlayer1 = tf.keras.layers.ConV2D(filters=24, kernel_size=3, 
+                                                    padding="same", activation="relu")
+        self.convlayer2 = tf.keras.layers.ConV2D(filters=24, kernel_size=3,
+                                                    padding="same", activation="relu")
+        self.pooling = tf.keras.layers.MaxPooling2D(pool_size=2, strides=2)
+
+        self.out = tf.keras.layers.Dense(10, activation="softmax")
+
+    def call(self, x):
+        x = self.conlayer1(x)
+        x = self.convlayer2(x)
+        x = self.pooling(x)
+        x = self.out(x)
+        return x
+
+
+def train_step(model, input, target, loss_function, optimizer):
+    with tf.GradientTape() as tape:
+        prediction = model(input)
+        loss = loss_function(target, prediction)
+    
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    
+    return loss
+
+
+def test(model, test_data, loss_function):
+    test_accuracy_aggregator = []
+    test_loss_aggregator = []
+
+    #eventuell numpy functions mit tf function ersetzen weil die schneller sind, ist aber nicht zwingend nötig
+
+    for (input, target) in test_data:
+        prediction = model(input)
+        sample_test_loss = loss_function(target, prediction)
+        sample_test_accuracy = np.argmax(target, axis=1) == np.argmax(prediction, axis=1)
+        sample_test_accuracy = np.mean(sample_test_accuracy)
+        test_loss_aggregator.append(sample_test_loss.numpy())
+        test_accuracy_aggregator.append(np.mean(sample_test_accuracy))
+
+    test_loss = tf.reduce_mean(test_loss_aggregator)
+    test_accuracy = tf.reduce_mean(test_accuracy_aggregator)
+    
+    return test_loss, test_accuracy
+
+
+#hyperparameters and variables for training loop
+epochs = 2
+eta = 0.001 #ist der Wert immer noch okay?
+num_neurons_hidden_layer = 30 #sind das immer noch hidden layers und brauchen wir 30?
+optimizer = tf.keras.optimizers.Adam(learning_rate=eta)
+
+model = Cifar10Model(num_neurons_hidden_layer) #neurons?
+cross_entropy_loss = tf.keras.losses.CategoricalCrossentropy()
+train_losses = []
+test_losses = []
+test_accs =[]
+
+
+#training loop
+for epoch in range(epochs):
+    print(f"Epoch: {str(epoch)}")
+
+    epoch_loss_agg = []
+
+    #was ist mit train_dataset und test_dataset? wie heißen die jetzt bzw wo kriegen wir die her?
+
+    for input, target in train_dataset:
+        train_loss = train_step(model, input, target, cross_entropy_loss, optimizer)
+        epoch_loss_agg.append(train_loss)
+
+    train_losses.append(tf.reduce_mean(epoch_loss_agg))
+
+    test_loss, test_accuracy = test(model, test_dataset, cross_entropy_loss)
+    test_losses.append(test_loss)
+    test_accs.append(test_accuracy)
